72c8dd795c44b9529da01b6e6ea5165e7abd0772
Cλstor <ygorcastor@gmail.com>
2026-05-08 18:00:34 +0200

    fix: Fixing Tidewave leaking on non-dev envs

--- lib/mnemosyne/application.ex
+++ lib/mnemosyne/application.ex
@@ -21,20 +21,24 @@ defmodule Mnemosyne.Application do
     Supervisor.start_link(children, opts)
   end
 
-  defp dev_children do
-    if System.get_env("TIDEWAVE_REPL") == "true" and Code.ensure_loaded?(Bandit) do
-      ensure_tidewave_started()
-      port = String.to_integer(System.get_env("TIDEWAVE_PORT", "10001"))
-      [{Bandit, plug: Tidewave, port: port}]
-    else
-      []
+  if Mix.env() == :dev do
+    defp dev_children do
+      if System.get_env("TIDEWAVE_REPL") == "true" and Code.ensure_loaded?(Bandit) do
+        ensure_tidewave_started()
+        port = String.to_integer(System.get_env("TIDEWAVE_PORT", "10001"))
+        [{Bandit, plug: Tidewave, port: port}]
+      else
+        []
+      end
     end
-  end
 
-  defp ensure_tidewave_started do
-    case Application.ensure_all_started(:tidewave) do
-      {:ok, _} -> :ok
-      {:error, _} -> :ok
+    defp ensure_tidewave_started do
+      case Application.ensure_all_started(:tidewave) do
+        {:ok, _} -> :ok
+        {:error, _} -> :ok
+      end
     end
+  else
+    defp dev_children, do: []
   end
 end
